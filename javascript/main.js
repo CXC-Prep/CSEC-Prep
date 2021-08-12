b530cadd4d5f055fed265928b33fe26be18ba65c
Malique_Auguste <maliqueauguste@gmail.com>
2021-08-12 13:46:18 -0400

    Update colours/themes.

--- javascript/main.js
+++ javascript/main.js
@@ -13,21 +13,17 @@ function init() {
 function change_theme() {
     let style = getComputedStyle(root);
 
-    let colour1 = style.getPropertyValue("--colour1");
-    let colour2 = style.getPropertyValue("--colour2");
-    let colour3 = style.getPropertyValue("--colour3");
-
     if(!dark) { 
-        root.style.setProperty("--colour1", colour2);
-        root.style.setProperty("--colour2", colour3);
-        root.style.setProperty("--colour3", colour1);
+        root.style.setProperty("--primary", style.getPropertyValue("--accent-colour"));
+        root.style.setProperty("--secondary", style.getPropertyValue("--light-colour"));
+        root.style.setProperty("--tertiary", style.getPropertyValue("--dark-colour"));
         dark = true;
         window.localStorage.setItem("theme", "dark");
     }
     else {
-        root.style.setProperty("--colour1", colour3);
-        root.style.setProperty("--colour2", colour1);
-        root.style.setProperty("--colour3", colour2);
+        root.style.setProperty("--primary", style.getPropertyValue("--light-colour"));
+        root.style.setProperty("--secondary", style.getPropertyValue("--dark-colour"));
+        root.style.setProperty("--tertiary", style.getPropertyValue("--accent-light"));
         dark = false;
         window.localStorage.setItem("theme", "light");
     }
