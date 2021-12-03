0dffb55755b8cecf870219544517d999f09e63e5
Malique_Auguste <maliqueauguste@gmail.com>
2021-12-02 21:00:55 -0400

    Added email collector

--- javascript/main.js
+++ javascript/main.js
@@ -1,6 +1,7 @@
 let dark = true;
 let root = document.querySelector(":root");
 let shareButtons = null;
+let email_getter, email_state;
 
 document.onload = init();
 
@@ -10,6 +11,7 @@ function init() {
         change_theme();
     }
     setTimeout(record_new_user, 2000)
+    setTimeout(email, 10000)
 }
 
 function change_theme() {
@@ -48,4 +50,65 @@ function record_new_user() {
 function record_share() {
     panelbear("track", "Shared")
     console.log("Share tracked")
+}
+
+function email() {
+    var email_state = window.localStorage.getItem("email-state");
+    var email_getter = document.getElementById("email-holder");
+
+    if(email_getter == null) {
+        console.log("index page")
+        return
+    }
+    
+    if (email_state == "received") {
+        console.log(email_state)
+    }
+    //checks if this is a new session
+    else if(window.sessionStorage.getItem("session")) {
+        console.log("ongoing")
+        return
+    }
+    else if (email_state == "denied") {
+        console.log(email_state)
+        denied_counter = parseInt(window.localStorage.getItem("denied-counter"));
+        console.log(denied_counter)
+
+        if(denied_counter > 0) {
+            window.localStorage.setItem("denied-counter", denied_counter - 1)
+        }
+        else {
+            email_getter.style.setProperty("display", "block");
+        
+            window.addEventListener('click', 
+                function(e){   
+                    if (!document.getElementById('email-form').contains(e.target)){
+                        email_getter.style.setProperty("display", "none");
+                        window.localStorage.setItem("denied-counter", 2)
+                    }
+                }
+            );
+        }
+    }
+    else {
+        console.log(email_state)
+        email_getter.style.setProperty("display", "block");
+        
+        window.addEventListener('click', 
+            function(e){   
+                if (!document.getElementById('email-form').contains(e.target)){
+                    email_getter.style.setProperty("display", "none");
+
+                    window.localStorage.setItem("email-state", "denied")
+                    window.localStorage.setItem("denied-counter", 2)
+                }
+            }
+        );
+    }
+
+    window.sessionStorage.setItem("session", "ongoing")
+}
+
+function received_email_state() {
+    window.localStorage.setItem("email-state", "received")
 }
